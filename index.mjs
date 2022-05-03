23dc27473158465b0a7a49949fb992991f3f07a6
pyro <90619153+pyroTM@users.noreply.github.com>
2022-05-02 23:14:06 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,5 +1,5 @@
 import Server from 'bare-server-node';
-import http from 'http';
+import https from 'https';
 import nodeStatic from 'node-static';
 import dotenv from 'dotenv';
 dotenv.config();
@@ -8,12 +8,22 @@ const bare = new Server('/bare/', '');
 const serve = new nodeStatic.Server('static/');
 const fakeServe = new nodeStatic.Server('BlacklistServe/');
 
-const server = http.createServer();
+const server = https.createServer();
+
+function setup(domains) {
+        for (const domain of domains)
+                server.addContext(domain, {
+                        key: fs.readFileSync(`/etc/letsencrypt/live/${domain}/privkey.pem`, 'utf8'),
+                        cert: fs.readFileSync(`/etc/letsencrypt/live/${domain}/cert.pem`, 'utf8')
+                });
+}
+
+const domains = ['development.lucidofficial.xyz'];
+
+setup(domains);
 
 server.on('request', (request, response) => {
-    /* Example env file: 
-    BlacklistedIPs="ip1,ip2,ip3"
-    */
+
   var BlacklistedIPs = process.env.BlacklistedIPs || "18.237.145.219,34.213.241.18,54.184.142.71,34.219.54.89,52.13.31.12,52.89.157.185,34.208.60.206,3.80.101.141,54.90.242.158,54.172.185.65,3.83.250.144,18.209.180.25,54.167.181.168,54.166.136.197, 52.207.207.52,54.252.242.153,3.104.121.59,34.253.198.121,63.33.56.11,34.250.114.219,54.171.251.199";
     let blacklist = BlacklistedIPs.split(",");
     var getClientIp = function (req) {
