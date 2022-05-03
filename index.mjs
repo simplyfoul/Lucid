abdf654fef014dac7622214349e489a183ee0727
pyro <90619153+pyroTM@users.noreply.github.com>
2022-05-03 17:59:04 -0500

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -4,7 +4,7 @@ import nodeStatic from 'node-static';
 import dotenv from 'dotenv';
 dotenv.config();
 const bare = new Server('/bare/', '');
-import fs from 'fs";
+import * as fs from 'fs';
 
 const serve = new nodeStatic.Server('static/');
 const fakeServe = new nodeStatic.Server('BlacklistServe/');
