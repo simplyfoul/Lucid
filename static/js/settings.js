73321f606ebd719b0204006e13f2a7c7e02bc0d3
Avad3 <jamesonmartin49@gmail.com>
2022-07-30 19:55:59 -0400

    Fix lag in settings menu (UV IS BROKEN)

--- static/js/settings.js
+++ static/js/settings.js
@@ -1,90 +1,55 @@
-var titleTab = document.getElementById("titleTab");
+const titleTab = document.getElementById("titleTab");
+const iconTab = document.getElementById("iconTab");
+const abc = document.getElementById("aboutBlankCloaking");
+const rhodium = document.getElementById('rhodium');
+const uv = document.getElementById('uv');
 
-var iconTab = document.getElementById("iconTab");
-
-
-function aboutBlankCloaking() {
-    if (document.getElementById("aboutBlankCloaking").checked) {
-
-        localStorage.setItem('aboutBlankCloaking', 'true');
-
-    } else {
-
-        localStorage.setItem('aboutBlankCloaking', 'false');
-    }
-
+if (localStorage.proxySelect === 'ultraviolet') {
+  uv.checked = true;
+  rhodium.checked = false;
+} else {
+  uv.checked = false;
+  rhodium.checked = true;
 }
 
+if (localStorage.aboutBlankCloaking === 'true') {
+  abc.checked = true;
+} else {
+  abc.checked = false;
+}
+
+function aboutBlankCloaking() {
+  if (abc.checked) {
+    localStorage.aboutBlankCloaking = 'true';
+  } else {
+    localStorage.aboutBlankCloaking = 'false';
+  }
+}
 
 function proxySelect() {
-    if (document.getElementById("uv").checked) {
-
-        localStorage.setItem('proxySelect', 'ultraviolet');
-
-    } else {
-
-        localStorage.setItem('proxySelect', 'rhodium');
-    }
-
+    localStorage.proxySelect = 'ultraviolet';
+    uv.checked = true;
+    rhodium.checked = false;
 }
 
 function proxySelect2() {
-    if (document.getElementById("rhodium").checked) {
-
-        localStorage.setItem('proxySelect', 'rhodium');
-
-    } else {
-
-        localStorage.setItem('proxySelect', 'ultraviolet');
-    }
-
+    localStorage.proxySelect = 'rhodium'
+    uv.checked = false;
+    rhodium.checked = true;
 }
 
-(function hello() {
-    if (localStorage.getItem('aboutBlankCloaking') === 'true') {
-
-        document.getElementById("aboutBlankCloaking").checked = true;
-
-
-    } else {
-        document.getElementById("aboutBlankCloaking").checked = false;
-    }
-
-    if (localStorage.getItem('proxySelect') === 'ultraviolet') {
-
-        document.getElementById("uv").checked = true;
-        document.getElementById('rhodium').checked = false;
-
-    } else if (localStorage.getItem('proxySelect') === 'rhodium') {
-
-
-        document.getElementById('rhodium').checked = true;
-        document.getElementById('uv').checked = false;
-
-    }
-
-    window.setInterval(hello, 100);
-
-
-})();
-
-
 if ((localStorage.getItem('tabTitle')) != null) {
-
     document.title = localStorage.getItem('tabTitle');
 }
 
 if (localStorage.getItem('tabIcon') != null) {
-
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
 
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
     link.href = localStorage.getItem('tabIcon');
 
     document.getElementsByTagName('head')[0].appendChild(link);
-
-
 }
 
 document.getElementById("tabTitle").addEventListener("click", function() {
@@ -98,7 +63,7 @@ document.getElementById("tabIcon").addEventListener("click", function() {
 
     localStorage.setItem('tabIcon', iconTab.value.trim());
 
-    var link = document.querySelector("link[rel*='icon']") || document.createElement('link');
+    let link = document.querySelector("link[rel*='icon']") || document.createElement('link');
 
     link.type = 'image/x-icon';
     link.rel = 'shortcut icon';
