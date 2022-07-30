b89e4e4b345afd2c806c16aba984081dacd18e93
Avad3 <65318266+Avad3@users.noreply.github.com>
2022-07-30 17:41:30 -0400

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,8 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-    content: [],
+    content: [
+        './static/**/*.{html,js}'
+    ],
     darkMode: 'class',
     daisyui: {
         themes: ["light", "dark", "cupcake", "bumblebee", "emerald", "corporate", "synthwave", "retro", "cyberpunk", "valentine", "halloween", "garden", "forest", "aqua", "lofi", "pastel", "fantasy", "wireframe", "black", "luxury", "dracula", "cmyk", "autumn", "business", "acid", "lemonade", "night", "coffee", "winter"],
